ae3ec4250dc7645dfdf516546e16286b9f3d9a99
Ajay patel <ajaypatelgurjar01@gmail.com>
2025-08-28 21:18:48 +0530

    new

--- Add to cart/project/project/urls.py
+++ Add to cart/project/project/urls.py
@@ -18,16 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.landing,name='landing'),
-    path('register/',views.register,name='register'),
-    path('login/',views.login,name='login'),
-    path('dashboard/',views.dashboard,name='dashboard'),
-    path('logout/',views.logout,name='logout'),
-    
-
-
-    
 ]
