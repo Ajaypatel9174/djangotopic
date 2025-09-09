56ce552ab527bff8e307d582709794456c28c0d6
Ajay patel <ajaypatelgurjar01@gmail.com>
2025-09-09 23:18:41 +0530

    new

--- drf based api/project/app/views.py
+++ drf based api/project/app/views.py
@@ -5,10 +5,29 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from.serializers import StudentSerializer
 from.models import Student
+import io
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def stu_list(req):
+    if req.method=='POST':
+        content = req.body
+        print(content)
+       
+
+        stream = io.BytesIO(content)
+        print(stream)
+        pdata = JSONParser().parse(stream)
+        print(pdata)
+        seralizer = StudentSerializer(data=pdata)
+        if seralizer.is_valid():
+            print(seralizer.validated_data)
+            seralizer.save()
+        else:
+            JsonResponse(seralizer.errors)
+       
     all_data = Student.objects.all()
     seralizer = StudentSerializer(all_data,many=True)
     print(seralizer.data)
@@ -19,4 +38,5 @@ def stu_list(req):
 
 
 def stu_detail(req,pk):
+
     pass
